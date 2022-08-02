7c4db0402f8c023db61d5baeb0b409ad3a54e6c7
CandidoVinii <vinicius.dias2020@engenharia.ufjf.br>
2022-08-01 21:20:15 -0300

    incrementando a imagen

--- docker/criando-imagen/index.js
+++ docker/criando-imagen/index.js
@@ -7,6 +7,6 @@ app.get("/", (req,res)=>{
     res.sendFile(__dirname + '/index.html')
 })
 
-app.listen("3000", ()=>{
+app.listen(process.env.PORT, ()=>{
     console.log("Server is listening on port 3000")
 })
